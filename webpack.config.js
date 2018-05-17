71d516fe4701e902d9f3c443b479c615d33e2183
Clement Renaud <clement@poex.io>
2018-05-17 14:24:40 +0200

    webpack watch+build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+
+const envPlugin = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+})
+
+const prod = process.env.NODE_ENV === 'production'
+
+
+const plugins = prod ?
+  [
+    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js','common.js'),
+    // new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 15}),
+    new webpack.optimize.MinChunkSizePlugin({minChunkSize: 10000}),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    envPlugin
+  ]
+  :
+  []
+
+const devtool = prod ? 'cheap-module-source-map' : 'inline-sourcemap'
+const watch = !prod
 
 module.exports = {
   entry: './lib/index.js',
+  devtool: devtool,
+  watch: watch,
+  mode : prod ? "production" : "development",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: "proofxApiClient.min.js"
   },
   module: {
     rules: [
