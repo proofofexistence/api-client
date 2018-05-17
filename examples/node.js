8bb6ae766312756224f49193c78784a386f4bad7
Clement Renaud <clement@poex.io>
2018-05-17 16:37:59 +0200

    add node js example

---
+++ examples/node.js
@@ -0,0 +1,28 @@
+var proofx = require('../lib')
+
+async function test() {
+  const hash = '15db6dbff590000ea13246e1c166802b690663c4e0635bfca78049d5a8762832'
+  let register = proofx.register(hash)
+  console.log(register.success) // true
+
+  let status = proofx.getStatus(hash)
+  status  = proofx.updateStatus(hash)
+  console.log(status.pending) // true
+
+  let docproofs = proofx.docproofs(hash)
+  console.log(docproofs.items) // []
+
+  // you can override the config (see axios config options)
+  let config = {baseUrl: 'https://proofofexistence.com'}
+  status = proofx.getStatus(hash, config)
+
+  return status
+}
+
+test()
+  .then(function (response) {
+    console.log(response)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
