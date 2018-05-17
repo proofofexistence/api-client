b81ba3907db6ba482f41998b5a1426083c4af218
Clement Renaud <clement@poex.io>
2018-05-17 16:41:08 +0200

    add basic state machine with state

--- test/index.js
+++ test/index.js
@@ -1,7 +1,27 @@
-var assert = require('assert');
+const assert = require('assert')
+
+const utils = require('../lib/utils')
 
 describe('Proof of Existence API Client', () => {
+
   it("should have tests", function(done){
     assert.equal(false, true)
+  })
+
+  describe('getDocStatus', function (done) {
+    it('should return the status of the document', function(){
+      assert.equal(
+        utils.getDocStatus({ pending : true, txstamp: null }),
+        'paymentRequired'
+      )
+      assert.equal(
+        utils.getDocStatus({txstamp : "Some Time" , blockstamp: null}),
+        'confirming'
+      )
+      assert.equal(
+        utils.getDocStatus({txstamp : "Some Time" , blockstamp: "89898"}),
+        'confirmed'
+      )
+    })
   })
 })
