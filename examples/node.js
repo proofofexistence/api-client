5fdb8c11d7a5336a8841b02f070bb95862621e35
Christopher Adams <christopher@poex.io>
2018-05-22 12:50:21 +0800

    Simplify node example

--- examples/node.js
+++ examples/node.js
@@ -2,48 +2,23 @@ var proofx = require('../lib')
 
 const hash = '15db6dbff590000ea13246e1c166802b690663c4e0635bfca78049d5a8762832'
 
-let register = proofx.register(hash)
-  .then(function (response) {
-    console.log(response.data)
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+async function test() {
+  registration = await proofx.register(hash)
+  status = await proofx.getStatus(hash)
 
-let status = proofx.getStatus(hash)
-  .then(function (response) {
-    console.log(response.data)
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-async function getStatus() {
   // you can override the config (see axios config options)
   let config = {baseUrl: 'https://proofofexistence.com'}
-  status = proofx.getStatus(hash, config)
 
-  return status
+  docproofs = await proofx.docproofs(hash, config)
+
+  return {status: status.data, docproofs: docproofs.data}
 }
 
-getStatus()
-  .then(function (response) {
-    console.log(response.data)
+test()
+  .then(function (results) {
+    console.log('Status:', results.status)
+    console.log('Docproofs:', results.docproofs)
   })
   .catch(function (error) {
     console.log(error);
-  });
-
-async function getDocproofs() {
-  let docproofs = proofx.docproofs(hash)
-  return docproofs
-}
-
-getDocproofs()
-  .then(function (response) {
-    console.log(response.data)
   })
-  .catch(function (error) {
-    console.log(error);
-  });
